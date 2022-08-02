3fadd06bc612def6c62c6cbac19c3a45662212f6
Kapkowy <88287407+Kapkowy@users.noreply.github.com>
2022-08-02 22:42:35 +0200

    Created main.py
    Here is source. you can edit it! if you want!

---
+++ main.py
@@ -0,0 +1,45 @@
+import functools
+import keyboard
+import PySimpleGUI as Sg
+import time
+import io
+
+menu_def = [['&File', ['Save As', 'Open', 'Save', 'E&xit']],
+            ['&Help', '&About...'], ]
+
+layout = [[Sg.Menu(menu_def, tearoff=True)],
+          [Sg.Multiline("", size=(85, 35), enable_events=True, visible=True, key='testek')],
+          ]
+
+rt = 'notepad'
+tr = Sg.popup_get_folder("Notepad STARTUP", "Please select folder!")
+if tr == '' or 'Quit':
+    print("THIS PROGRAM CANT WORKING IF YOU DONT SELECT WHERE YOU WANT TO SAVE (ERROR)")
+elif tr != '':
+    print("you select " + tr)
+
+window = Sg.Window('Notepad python', layout, icon='icon.ico')
+
+while True:
+    event, values = window.read()
+    if event == Sg.WINDOW_CLOSED or event == 'Exit':
+        break
+    if event == 'New':
+        window['testek'].update("Start Writing")
+    elif event == 'Open':
+        ter = open(Sg.popup_get_file("", file_types=(("Text Files", "*.txt"),)))
+        window['testek'].update(ter.read())
+    elif event == 'Save As':
+        rt = Sg.popup_get_text('Name of file without .txt', "Notepad", icon='icon.ico')
+        tr = Sg.popup_get_folder('Where you want to save?', "Notepad", icon='icon.ico')
+        with io.open(tr + '/' + rt + ".txt", "a", encoding="utf8") as f:
+            f.write(values['testek'])
+            f.write('\n')
+        f.close()
+    elif keyboard.is_pressed("ctrl+s"):
+        with io.open(tr + '/' + rt + ".txt", "w", encoding="utf8") as f:
+            f.write(values['testek'])
+        f.close()
+    elif event == 'About...':
+        import About
+window.close()
